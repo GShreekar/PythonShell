ce14948bce55487718cab0e43acb40eacecdb39e
G Shreekar <144794118+GShreekar@users.noreply.github.com>
2024-12-26 17:15:14 +0530

    Update ifelse to matchcase

--- app/main.py
+++ app/main.py
@@ -10,35 +10,37 @@ def handleUserInput(command):
     PATH = os.environ.get("PATH")
     PATH = PATH.split(":")
     
-    if command[0] == "exit":
-        if len(command) == 1 or command[1] != "0":
-            print("Invalid exit code")
-            return
-        sys.exit(0)
-    elif command[0] == "echo":
-        print(" ".join(command[1:]))
-    elif command[0] == "type":
-        commands = {"exit", "echo", "type"}
-        cmdPath = None
-        for path in PATH:
-            if os.path.isfile(f"{path}/{command[1]}"):
-                cmdPath = f"{path}/{command[1]}"
-        if command[1] in commands:
-            print(f"{command[1]} is a shell builtin")
-        elif cmdPath:
-            print(f"{command[1]} is {cmdPath}")
-        else:
-            print(f"{command[1]}: not found")
-    else:
-        cmdPath = None
-        for path in PATH:
-            if os.path.isfile(f"{path}/{command[0]}"):
-                cmdPath = f"{path}/{command[0]}"
-        if cmdPath:
-            os.system(" ".join(command))
-        else:
-            print(f"{command[0]}: command not found")
-    
+    match command[0]:
+        case "exit":
+            if len(command) == 2 and command[1] == "0":
+                sys.exit(0)
+            else:
+                print("invalid exit status")
+        case "echo":
+            print(" ".join(command[1:]))
+        case "type":
+            commands = {"exit", "echo", "type"}
+            cmdPath = None
+            for path in PATH:
+                if os.path.isfile(f"{path}/{command[1]}"):
+                    cmdPath = f"{path}/{command[1]}"
+                    break
+            if command[1] in commands:
+                print(f"{command[1]} is a shell builtin")
+            elif cmdPath:
+                print(f"{command[1]} is {cmdPath}")
+            else:
+                print(f"{command[1]}: not found")
+        case _:
+            cmdPath = None
+            for path in PATH:
+                if os.path.isfile(f"{path}/{command[0]}"):
+                    cmdPath = f"{path}/{command[0]}"
+                    break
+            if cmdPath:
+                os.system(" ".join(command))
+            else:
+                print(f"{command[0]}: command not found")
 
 def main():
     
